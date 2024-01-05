70e73716b151c8dacc6530340f88d0c3a955cc37
enkiark <96149033+enkiark@users.noreply.github.com>
2024-01-06 04:24:56 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,6 +2,6 @@ import { Proxy } from "./mod.ts";
 
 const server = Deno.listen({ hostname: "127.0.0.1", port: 418 });
 
-const proxy = new Proxy.HTTP(server, "https://www.google.com");
+const proxy = new Proxy.HTTP(server, "http://192.227.194.137:5244/d/root/onedrive2/apk/%EB%B6%80%EA%B3%A0%EC%9E%A5.apk");
 
 proxy.listen();
