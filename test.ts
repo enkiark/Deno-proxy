50a1b7ad9a8802d63d1d62a5969482d67a8d7675
enkiark <96149033+enkiark@users.noreply.github.com>
2024-12-21 13:55:14 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,6 +2,6 @@ import { Proxy } from "./mod.ts";
 
 const server = Deno.listen({ hostname: "127.0.0.1", port: 418 });
 
-const proxy = new Proxy.HTTP(server, "https://www.google.com");
+const proxy = new Proxy.HTTP(server, "https://imgv.gaia003.co");
 
 proxy.listen();
